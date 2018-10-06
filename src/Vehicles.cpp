fc6498f4250ec277f6c16749f5db55b1130ae089
Andrew Che <andrew@codeninja55.me>
2018-10-06 21:43:01 +1000

    Added member vars for stats and add_stats() func to assign them

--- src/Vehicles.cpp
+++ src/Vehicles.cpp
@@ -1,31 +1,60 @@
 /*********************************************************************************
-* CSCI262 - Assignment 03
-* Traffic Activity Engine
-* Vehicles.cpp
-* - Implementation for Vehicles wrapper class to be used as a storage container
-*   for Vehicles struct in a map based on integer id keys.
+* CSCI262 (Spring 2018) - Assignment 03
+* Pooh Bear Intrusion Detection System Vehicles.cpp
+* Purpose: Implementation for Vehicles wrapper class to be used as a storage
+*          container for Vehicles struct in a map based on integer id keys.
 *
-* Version: 0.1-dev
-* Last modified: 2018.10.06
+* @version 0.1-dev 2018.10.06
 *
-* Authors (Github) | email:
-*   - Dinh Che (codeninja55) | andrew at codeninja55.me
-*   - Duong Le (daltonle) |  duong.daltonle at gmail.com
-* UOW Details:
-*   - Dinh Che (5721970) | dbac496 at uowmail.edu.au
-*   - Duong Le (5560536) | ndl991 at uowmail.edu.au
+* @authors Dinh Che (codeninja55) & Duong Le (daltonle)
+* Emails: andrew at codeninja55.me & duong.daltonle at gmail.com
+* StudentsS Dinh Che (5721970 | dbac496) & Duong Le (5560536 | ndl991)
 *********************************************************************************/
 
 #include <iomanip>
 #include "Vehicles.h"
 
-void Vehicles::insert(Vehicle &v)
+/*
+ * A wrapper class function to inser the VehicleType structure into a hash map
+ * with the VehicleType name as the key.
+ *
+ * @param v: a passed-by-reference VehicleType structure to be stored.
+ * */
+void Vehicles::insert(VehicleType &v)
 {
     v.id = ++n_vehicles;
-    map<string, Vehicle>::iterator iter = vehicles_dict.find(v.name);
-    vehicles_dict.insert(iter, pair<string, Vehicle>(v.name, v));
+    map<string, VehicleType>::iterator iter = vehicles_dict.find(v.name);
+    vehicles_dict.insert(iter, pair<string, VehicleType>(v.name, v));
 }
 
+/*
+ * A function to add the stats for each type by finding them in the hash map first and storing
+ * the values in the VehicleType structure variables.
+ *
+ * @param name: the name of the type to find in the vehicles_dict key.
+ * @param num_mean: an unsigned int to represent the mean for number of that vehicle type.
+ * @param num_stddev: an unsigned int to represent the standard deviation for that vehicle type.
+ * @param speed_mean: an unsigned int to represent the mean speed for that vehicle type.
+ * @param speed_stddev: an unsigned int to represent the standard deviation speed for that vehicle type.
+ * @return: bool true if the vehicle type can be found by name key or false if otherwise.
+ * */
+bool Vehicles::add_stats(string name, uint num_mean, uint num_stdev, uint speed_mean, uint speed_stddev)
+{
+    map<string, VehicleType>::iterator iter = vehicles_dict.find(name);
+
+    if (iter != vehicles_dict.end()) {
+        (*iter).second.num_mean = num_mean;
+        (*iter).second.num_stddev = num_stdev;
+        (*iter).second.speed_mean = speed_mean;
+        (*iter).second.speed_stddev = speed_stddev;
+        return true;
+    }
+    return false;
+}
+
+/*
+ * Print the stored Vehicle types in a prettified table.
+ * */
 void Vehicles::print()
 {
     cout <<"| "<<right<<setw(3)<<"ID"<<" | "<<left<<setw(15)<<"Name"
@@ -35,13 +64,13 @@ void Vehicles::print()
     cout <<"|"<<setfill('-')<<setw(5)<<"-"<<"|"<<setw(17)<< "-"<<"|"<<setw(9)<<"-"
          <<"|"<<setw(13)<<"-"<<"|"<<setw(15)<<"-"<<"|"<<setw(14)<<"-"<<setfill(' ')<<"|"<<left<<endl;
 
-    map<string, Vehicle>::iterator iter = vehicles_dict.begin();
+    map<string, VehicleType>::iterator iter = vehicles_dict.begin();
     for (; iter != vehicles_dict.end(); iter++) {
         string parking = ((*iter).second.parking_flag) ? "Yes" : "No";
         cout <<"|"<<right<<setw(4)<< (*iter).second.id <<" | "<<left<<setw(15)<< (*iter).first
              <<" | "<<left<<setw(7)<< parking <<" | "<<left<<setw(11)<< (*iter).second.reg_format
              <<" | "<<right<<setw(13)<< (*iter).second.vol_weight <<" | "<<setw(12)<< (*iter).second.speed_weight
-             <<" |"<<left<<endl;
+             <<" |"<<left<<"\n"<<flush;
     }
+    cout<<endl;
 }
-
--- src/Vehicles.h
+++ src/Vehicles.h
@@ -1,30 +1,42 @@
-//
-// Created by codeninja on 6/10/18.
-//
+/*********************************************************************************
+* CSCI262 (Spring 2018) - Assignment 03
+* Pooh Bear Intrusion Detection System Vehicles.h
+* Purpose: Header file for Vehicles class and structure definition of Vehicle
+*
+* @version 0.1-dev 2018.10.06
+*
+* @authors Dinh Che (codeninja55) & Duong Le (daltonle)
+* Emails andrew at codeninja55.me & duong.daltonle at gmail.com
+* Students Dinh Che (5721970 | dbac496) & Duong Le (5560536 | ndl991)
+*********************************************************************************/
 
-#ifndef TRAFFIC_ACTIVITY_ENGINE_VEHICLES_H
-#define TRAFFIC_ACTIVITY_ENGINE_VEHICLES_H
+#ifndef POOH_BEAR_INTRUSION_DETECTION_SYSTEM_VEHICLES_H
+#define POOH_BEAR_INTRUSION_DETECTION_SYSTEM_VEHICLES_H
 
 #include <string>
 #include <map>
 #include <iostream>
+#include <zconf.h>
 using namespace std;
 
 typedef struct {
     int id;
     string name, reg_format;
     bool parking_flag;
-    unsigned int vol_weight, speed_weight;
-} Vehicle;
+    uint vol_weight, speed_weight;
+    uint num_mean, num_stddev, speed_mean, speed_stddev;
+} VehicleType;
 
 class Vehicles {
 public:
     Vehicles() : n_vehicles(0) {};
-    void insert(Vehicle &v);
+    void insert(VehicleType&);
+    bool add_stats(string, uint, uint, uint, uint);
     void print();
+
 private:
-    unsigned int n_vehicles;
-    map<string, Vehicle> vehicles_dict;
+    uint n_vehicles;
+    map<string, VehicleType> vehicles_dict;
 };
 
-#endif //TRAFFIC_ACTIVITY_ENGINE_VEHICLES_H
+#endif //POOH_BEAR_INTRUSION_DETECTION_SYSTEM_VEHICLES_H
