a055d35637cf55ed59655c0c4225ecdd4485ada6
Unknown <duong.daltonle@gmail.com>
2018-10-17 14:01:11 +1100

    Modify ActivityEngine::run() to log road length and speed limit
    Analysis Engine needs to read these from the log

--- src/AnalysisEngine.h
+++ src/AnalysisEngine.h
@@ -7,14 +7,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <map>
 #include "Utils.h"
 #include "Vehicles.h"
 #include "Logger.h"
 using namespace std;
 
+typedef struct {
+    
+} AnalysisStats;
+
 class AnalysisEngine {
 public:
+    AnalysisEngine();
+    AnalysisEngine(char* filename);
+
 private:
+    map<string, VehicleType> curr_vehicles;
 };
 
 #endif //POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ANALYSISENGINE_H
