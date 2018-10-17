74b2ac8127b52b87302787167bec54b705c55419
Unknown <duong.daltonle@gmail.com>
2018-10-17 12:21:20 +1100

    Analysis created

---
+++ src/AnalysisEngine.cpp
@@ -0,0 +1,4 @@
+//
+// Created by lnduo on 17/10/18.
+//
+
---
+++ src/AnalysisEngine.h
@@ -0,0 +1,20 @@
+//
+// Created by lnduo on 17/10/18.
+//
+
+#ifndef POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ANALYSISENGINE_H
+#define POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ANALYSISENGINE_H
+
+#include <iostream>
+#include <fstream>
+#include "Utils.h"
+#include "Vehicles.h"
+#include "Logger.h"
+using namespace std;
+
+class AnalysisEngine {
+public:
+private:
+};
+
+#endif //POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ANALYSISENGINE_H
