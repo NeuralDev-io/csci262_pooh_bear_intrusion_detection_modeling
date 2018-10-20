02b51b837692eb47224d87822389e8f72a5c7e68
Andrew Che <andrew@codeninja55.me>
2018-10-21 00:15:50 +1100

    Added AlertEngine class for Alert phase

---
+++ src/AlertEngine.h
@@ -0,0 +1,26 @@
+/*********************************************************************************
+* CSCI262 (Spring 2018) - Assignment 03
+* Pooh Bear Intrusion Detection System AlertEngine.h
+* Purpose: Header file for AlertEngine class.
+*
+* @version 0.6-dev
+* @date 2018.10.20
+*
+* @authors Dinh Che (codeninja55) & Duong Le (daltonle)
+* Emails andrew at codeninja55.me & duong.daltonle at gmail.com
+* Students Dinh Che (5721970 | dbac496) & Duong Le (5560536 | ndl991)
+*********************************************************************************/
+
+#ifndef POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ALERTENGINE_H
+#define POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ALERTENGINE_H
+
+class AlertEngine {
+
+public:
+    void run();
+private:
+
+
+};
+
+#endif //POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ALERTENGINE_H
