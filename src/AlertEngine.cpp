5f6a514902b157f54075ed06efba4dcfe70d0852
Andrew Che <andrew@codeninja55.me>
2018-10-21 02:57:21 +1100

    Completed skeletons of Alert Engine

--- src/AlertEngine.cpp
+++ src/AlertEngine.cpp
@@ -1,10 +1,11 @@
 /*********************************************************************************
 * CSCI262 (Spring 2018) - Assignment 03
 * Pooh Bear Intrusion Detection System AlertEngine.h
-* Purpose: Creats an Alert Engine object that
+* Purpose: Creates an Alert Engine object that checks thresholds for vehicles
+*          and alerts if anomaly counters are over these thresholds.
 *
-* @version 0.6-dev
-* @date 2018.10.20
+* @version 0.7-dev
+* @date 2018.10.21
 *
 * @authors Dinh Che (codeninja55) & Duong Le (daltonle)
 * Emails andrew at codeninja55.me & duong.daltonle at gmail.com
@@ -13,7 +14,13 @@
 
 #include "AlertEngine.h"
 
-void AlertEngine::run()
+AlertEngine::AlertEngine(string log_file, string data_file)
+{
+    log_file = log_file;
+    alert_logger = Logger<SimTime, AlertLog>("Alert Engine", INFO, log_file, true);
+}
+
+void AlertEngine::run(Vehicles &vehicles, int &days)
 {
 
 }
--- src/AlertEngine.h
+++ src/AlertEngine.h
@@ -1,3 +1,5 @@
+#include <utility>
+
 /*********************************************************************************
 * CSCI262 (Spring 2018) - Assignment 03
 * Pooh Bear Intrusion Detection System AlertEngine.h
@@ -14,13 +16,44 @@
 #ifndef POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ALERTENGINE_H
 #define POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ALERTENGINE_H
 
+#include <string>
+#include "Logger.h"
+#include "Vehicles.h"
+#include "Utils.h"
+
+using namespace std;
+
+typedef struct AlertStats {
+    SimTime day;
+    double volume_anomaly_ctr_threshold;
+    double speed_anomaly_ctr_threshold;
+    double daily_volume_anomaly_ctr;
+    double daily_speed_anomaly_ctr;
+} AlertStats;
+
+
+typedef struct AlertLog {
+    string ev_type = "ALERT";
+    string log_name = "Alert Log";
+    string msg = "";
+
+    AlertLog(string msg) : msg(move(msg)) { }
+
+    friend ostream& operator<<(ostream& os, AlertLog const &alert_log)
+    {
+        return os << alert_log.log_name << DELIMITER << alert_log.ev_type
+                  << DELIMITER << alert_log.msg;
+    }
+} AlertLog;
+
 class AlertEngine {
-
 public:
-    void run();
+    explicit AlertEngine(string log_file, string data_file);
+    void run(Vehicles &vehicles, int &days);
 private:
-
-
+    Logger<SimTime, AlertLog> alert_logger;
+    string log_file;
+    string data_file;
 };
 
 #endif //POOH_BEAR_INTRUSION_DETECTION_SYSTEM_ALERTENGINE_H
